3061a2d154512c751e66c3858880feb6f549a98c
ichthus1604 <ichthus1604@gmail.com>
2023-02-09 21:07:50 -0300

    Added NavigationMetadata to ConfirmHideAddressViewModel

--- WalletWasabi.Fluent/ViewModels/Dialogs/ConfirmHideAddressViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Dialogs/ConfirmHideAddressViewModel.cs
@@ -5,14 +5,12 @@ using WalletWasabi.Wallets;
 
 namespace WalletWasabi.Fluent.ViewModels.Dialogs;
 
-public class ConfirmHideAddressViewModel : DialogViewModelBase<bool>
+[NavigationMetaData(Title = "Hide Address")]
+public partial class ConfirmHideAddressViewModel : DialogViewModelBase<bool>
 {
-	private string _title;
-
 	public ConfirmHideAddressViewModel(Address address)
 	{
 		Label = address.Labels;
-		_title = "Hide Address";
 
 		NextCommand = ReactiveCommand.Create(() => Close(result: true));
 		CancelCommand = ReactiveCommand.Create(() => Close(DialogResultKind.Cancel));
@@ -21,10 +19,4 @@ public class ConfirmHideAddressViewModel : DialogViewModelBase<bool>
 	}
 
 	public IEnumerable<string> Label { get; }
-
-	public override string Title
-	{
-		get => _title;
-		protected set => this.RaiseAndSetIfChanged(ref _title, value);
-	}
 }
