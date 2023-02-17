0d3bf03e9c8c85b500cf80da7e917fb70024c9f3
José Manuel Nieto <superjmn@outlook.com>
2023-02-17 17:01:43 +0100

    Simplify ReceiveAddressesViewModel

--- WalletWasabi.Fluent/ViewModels/Wallets/Receive/ReceiveAddressesViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Receive/ReceiveAddressesViewModel.cs
@@ -1,13 +1,10 @@
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using Avalonia.Controls;
-using Avalonia.Controls.Models.TreeDataGrid;
-using Avalonia.Controls.Templates;
 using DynamicData;
 using WalletWasabi.Fluent.Models.Wallets;
 using WalletWasabi.Fluent.ViewModels.Dialogs.Base;
 using WalletWasabi.Fluent.ViewModels.Navigation;
-using WalletWasabi.Fluent.Views.Wallets.Receive.Columns;
 
 namespace WalletWasabi.Fluent.ViewModels.Wallets.Receive;
 
@@ -34,65 +31,14 @@ public partial class ReceiveAddressesViewModel : RoutableViewModel
 			.Bind(out _addresses)
 			.Subscribe();
 
-		Source = new FlatTreeDataGridSource<AddressViewModel>(_addresses)
-		{
-			Columns =
-			{
-				ActionsColumn(),
-				AddressColumn(),
-				LabelsColumn()
-			}
-		};
+		Source = ReceiveAddressesDataGridSource.Create(_addresses);
 
 		Source.RowSelection!.SingleSelect = true;
 	}
 
-	public IWalletModel Wallet { get; }
+	private IWalletModel Wallet { get; }
 
 	public FlatTreeDataGridSource<AddressViewModel> Source { get; }
-
-	private static IColumn<AddressViewModel> ActionsColumn()
-	{
-		return new TemplateColumn<AddressViewModel>(
-			null,
-			new FuncDataTemplate<AddressViewModel>((node, ns) => new ActionsColumnView(), true),
-			options: new ColumnOptions<AddressViewModel>
-			{
-				CanUserResizeColumn = false,
-				CanUserSortColumn = false
-			},
-			width: new GridLength(90, GridUnitType.Pixel));
-	}
-
-	private static IColumn<AddressViewModel> AddressColumn()
-	{
-		return new TemplateColumn<AddressViewModel>(
-			"Address",
-			new FuncDataTemplate<AddressViewModel>((node, ns) => new AddressColumnView(), true),
-			options: new ColumnOptions<AddressViewModel>
-			{
-				CanUserResizeColumn = false,
-				CanUserSortColumn = true,
-				CompareAscending = AddressViewModel.SortAscending(x => x.Address),
-				CompareDescending = AddressViewModel.SortDescending(x => x.Address)
-			},
-			width: new GridLength(2, GridUnitType.Star));
-	}
-
-	private static IColumn<AddressViewModel> LabelsColumn()
-	{
-		return new TemplateColumn<AddressViewModel>(
-			"Labels",
-			new FuncDataTemplate<AddressViewModel>((node, ns) => new LabelsColumnView(), true),
-			options: new ColumnOptions<AddressViewModel>
-			{
-				CanUserResizeColumn = false,
-				CanUserSortColumn = true,
-				CompareAscending = AddressViewModel.SortAscending(x => x.Label),
-				CompareDescending = AddressViewModel.SortDescending(x => x.Label)
-			},
-			width: new GridLength(210, GridUnitType.Pixel));
-	}
 
 	private AddressViewModel CreateAddressViewModel(IAddress address)
 	{
