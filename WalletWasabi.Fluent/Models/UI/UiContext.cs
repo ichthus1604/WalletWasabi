f5259625ed606dc1be6687a063b387e2cf61fdc3
José Manuel Nieto <superjmn@outlook.com>
2023-04-04 11:35:02 +0200

    Don't recreate Default instance

--- WalletWasabi.Fluent/Models/UI/UiContext.cs
+++ WalletWasabi.Fluent/Models/UI/UiContext.cs
@@ -6,35 +6,36 @@ namespace WalletWasabi.Fluent.Models.UI;
 
 public class UiContext
 {
-	private INavigate? _navigate;
+    private INavigate? _navigate;
+    private static UiContext? DefaultInstance;
 
-	public UiContext(IQrCodeGenerator qrCodeGenerator, IClipboard clipboard)
-	{
-		QrCodeGenerator = qrCodeGenerator ?? throw new ArgumentNullException(nameof(qrCodeGenerator));
-		Clipboard = clipboard ?? throw new ArgumentNullException(nameof(clipboard));
-	}
+    public UiContext(IQrCodeGenerator qrCodeGenerator, IClipboard clipboard)
+    {
+        QrCodeGenerator = qrCodeGenerator ?? throw new ArgumentNullException(nameof(qrCodeGenerator));
+        Clipboard = clipboard ?? throw new ArgumentNullException(nameof(clipboard));
+    }
 
-	public IClipboard Clipboard { get; }
-	public IQrCodeGenerator QrCodeGenerator { get; }
+    public IClipboard Clipboard { get; }
+    public IQrCodeGenerator QrCodeGenerator { get; }
 
-	// The use of this property is a temporary workaround until we finalize the refactoring of all ViewModels (to be testable)
-	// We provide a NullClipboard object for unit tests (when Application.Current is null)
-	public static UiContext Default => new(new QrGenerator(), Application.Current?.Clipboard ?? new NullClipboard());
+    // The use of this property is a temporary workaround until we finalize the refactoring of all ViewModels (to be testable)
+    // We provide a NullClipboard object for unit tests (when Application.Current is null)
+    public static UiContext Default => DefaultInstance ??= new UiContext(new QrGenerator(), Application.Current?.Clipboard ?? new NullClipboard());
 
-	public void RegisterNavigation(INavigate navigate)
-	{
-		_navigate ??= navigate;
-	}
+    public void RegisterNavigation(INavigate navigate)
+    {
+        _navigate ??= navigate;
+    }
 
-	public INavigate Navigate()
-	{
-		return _navigate ?? throw new InvalidOperationException($"{GetType().Name} {nameof(_navigate)} hasn't been initialized.");
-	}
+    public INavigate Navigate()
+    {
+        return _navigate ?? throw new InvalidOperationException($"{GetType().Name} {nameof(_navigate)} hasn't been initialized.");
+    }
 
-	public INavigationStack<RoutableViewModel> Navigate(NavigationTarget target)
-	{
-		return
-			_navigate?.Navigate(target)
-			?? throw new InvalidOperationException($"{GetType().Name} {nameof(_navigate)} hasn't been initialized.");
-	}
+    public INavigationStack<RoutableViewModel> Navigate(NavigationTarget target)
+    {
+        return
+            _navigate?.Navigate(target)
+            ?? throw new InvalidOperationException($"{GetType().Name} {nameof(_navigate)} hasn't been initialized.");
+    }
 }
